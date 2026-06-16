401bd76b76a97f70afce4cf0d073ff5381722e9b
jamesCarl1204 <jamesmagsipoc887@gmail.com>
2026-06-16 10:38:36 +0800

    fix script

--- grade-tracker/script.js
+++ grade-tracker/script.js
@@ -5,62 +5,62 @@
             {name :"ben", grade: 60},
             {name:"mia", grade: 45},
             {name:"leo", grade: 78}
-        ]
+        ];
 
-        let student = " "
+        let student = " ";
 
-        for(let v in rawData){
+        for(let v in rawData) {
             student += rawData[v].name + " ";
         }
 
-        document.getElementById("outputStudent").innerHTML = student
+        document.getElementById("outputStudent").innerHTML = student;
     
-        let average = 0
+        let average = 0;
 
         for(let i in rawData) {
-            average += rawData[i].grade
+            average += rawData[i].grade;
         }
-        average /= 5
+        average /= 5;
 
-        document.getElementById("average").innerHTML = average
+        document.getElementById("average").innerHTML = average;
 
         function mycallback({grade}) {
-            return grade >= 75 ? "passed" : "failed"
+            return grade >= 75 ? "passed" : "failed";
         }
 
-         let result = Object.groupBy(rawData, mycallback)
+         let result = Object.groupBy(rawData, mycallback);
         
 
-         let passed = ""
+         let passed = "";
 
-         for(let x of result.passed){
-            passed += x.name + " "
+         for(let x of result.passed) {
+            passed += x.name + " ";
          }
 
-         document.getElementById("output").innerHTML = passed
+         document.getElementById("output").innerHTML = passed;
           
-         let failed = ""
+         let failed = "";
 
          for(let x of result.failed) {
-            failed += x.name + " "
+            failed += x.name + " ";
          }
 
-        document.getElementById("outputFailed").innerHTML = failed
+        document.getElementById("outputFailed").innerHTML = failed;
          
         })();
 
          function createTracker() {
-            let access = 0
+            let access = 0;
             return function add() {
-                access++
-                return "Grade checked " + access + (access > 1 ? " times" : " time")
+                access++;
+                return "Grade checked " + access + (access > 1 ? " times" : " time");
             }
          }
 
-         const tracker = createTracker()
+         const tracker = createTracker();
          
          function checkGrades() {
-           document.getElementById("check").innerHTML = tracker()
+           document.getElementById("check").innerHTML = tracker();
          }
         
         console.log()
